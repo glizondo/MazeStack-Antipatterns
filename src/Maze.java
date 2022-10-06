156d579f6df9968a7e4727bfb4f5a7289970464f
glizondo <80534057+glizondo@users.noreply.github.com>
2022-10-06 15:41:49 -0400

    Added class Maze with variables and constructor

--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,17 @@
 
 public class Maze {
 
+	private int x;
+	private int y;
+	private int[][] startPoint;
+	private int[][] endPoint;
+	private int[][] currentPoint;
+
+	public Maze(int x, int y, int[][] startPoint, int[][] endPoint) {
+		this.x = x;
+		this.y = y;
+		this.startPoint = startPoint;
+		this.endPoint = endPoint;
+	}
+
 }
